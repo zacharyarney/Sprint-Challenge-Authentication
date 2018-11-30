fab57407d5a2031d04ef8ce6dc6e4ce9587db65c
Unknown <zachary.arney@gmail.com>
2018-11-30 11:27:08 -0600

    login POST endpoint

--- config/routes.js
+++ config/routes.js
@@ -46,6 +46,22 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome!', token });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'authentication failed', err });
+    });
 }
 
 function getJokes(req, res) {
